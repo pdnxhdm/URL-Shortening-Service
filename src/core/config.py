df21b321269b1bc3b7449300e182ec8494043837
Иван Калинкин <vkalinkin372@gmail.com>
2026-04-17 19:41:52 +0300

    feat: add processing .env file

---
+++ src/core/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Config(BaseSettings):
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5432
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+    @property
+    def DB_URL(self) -> str:
+        return f"postgresql://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+
+config = Config()
